cbb5b20788a73ad7186f1d6538e791a27665dd5f
Alessandro Vinci <alessandrovinciabc@gmail.com>
2020-09-19 11:46:34 +0200

    Add song card's skeleton

--- src/index.js
+++ src/index.js
@@ -4,14 +4,24 @@ import * as serviceWorker from './serviceWorker';
 
 //Bootstrap
 import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
 
 ReactDOM.render(
-    <Navbar bg="dark" className="main-navbar">
-        <Navbar.Brand className="navbar-logo text-primary">
-            <i class="far fa-laugh-beam navbar-logo--image"></i>mbitbit
-        </Navbar.Brand>
-    </Navbar>
-    ,
+    <React.Fragment>
+        <Navbar bg="dark" className="main-navbar">
+            <Navbar.Brand className="navbar-logo text-primary">
+                <i className="far fa-laugh-beam navbar-logo--image"></i>mbitbit
+            </Navbar.Brand>
+        </Navbar>
+        <Container className="main-container">
+            <Card className="text-center">
+                <Card.Header>Name of the song...</Card.Header>
+                <Card.Body>
+                </Card.Body>
+            </Card>
+        </Container>
+    </React.Fragment>,
     document.getElementById('root')
 );
 
