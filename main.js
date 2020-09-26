b3955bf3d5c466d3ddd77114ed22e01268221b20
Alessandro Vinci <alessandrovinciabc@gmail.com>
2020-09-26 05:15:27 +0200

    Add tracks, route to get list of songs

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
@@ -6,10 +7,26 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/api/songlist', (req, res) => {
+    const musicDirectory = './build/tracks/';
+
+    fs.readdir(musicDirectory, (err, files) => {
+        if (err) {
+            throw err;
+        }
+
+        res.json(files);
+    });
+});
+
+app.get('/:songName', (req, res) => {
+    res.json({ link: `/tracks/${req.params.songName}` });
+});
+
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}...`);
-})
+    console.log(`Listening at http://localhost:${port}...`);
+});
--- src/index.js
+++ src/index.js
@@ -8,19 +8,29 @@ import Container from 'react-bootstrap/Container';
 import Header from './Header';
 import MusicPlayer from './MusicPlayer';
 
-ReactDOM.render(
-    <React.Fragment>
-        <Header />
-        <Container className="main-container">
-            <MusicPlayer
-                songName="Tipplechipper"
-                authorName="..."
-                songSrc="./tracks/tipplechipper.wav"
-            />
-        </Container>
-    </React.Fragment>,
-    document.getElementById('root')
-);
+async function main() {
+    const songListRequest = await fetch('http://localhost:3000/api/songlist');
+    const availableSongs = await songListRequest.json();
+
+    ReactDOM.render(
+        <React.Fragment>
+            <Header />
+            <Container className="main-container">
+                <ul>
+                    {availableSongs.map((song)=>(<li>{song}</li>))}
+                </ul>
+                <MusicPlayer
+                    songName="Tipplechipper"
+                    authorName="..."
+                    songSrc="./tracks/tipplechipper.wav"
+                />
+            </Container>
+        </React.Fragment>,
+        document.getElementById('root')
+    );
+}
+
+main();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
